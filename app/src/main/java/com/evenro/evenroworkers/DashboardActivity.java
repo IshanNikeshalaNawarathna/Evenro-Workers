f08561712c948af8583455ed6b51d274d8e32cf3
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-20 15:11:13 +0530

    invoice history completed

--- app/src/main/java/com/evenro/evenroworkers/DashboardActivity.java
+++ app/src/main/java/com/evenro/evenroworkers/DashboardActivity.java
@@ -42,7 +42,7 @@ public class DashboardActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.allUsersActivity, R.id.allEventActivity)
+                R.id.allUsersActivity, R.id.allEventActivity,R.id.invoiceHistoryActivity)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard);
---
+++ app/src/main/java/com/evenro/evenroworkers/ui/invoiceHistory/InvoiceHistoryActivity.java
@@ -0,0 +1,82 @@
+package com.evenro.evenroworkers.ui.invoiceHistory;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.evenro.evenroworkers.R;
+import com.evenro.evenroworkers.ui.adapter.InvoiceAdapter;
+import com.evenro.evenroworkers.ui.model.InvoiceData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class InvoiceHistoryActivity extends AppCompatActivity {
+    private ArrayList<InvoiceData> fullEventList;
+    private RecyclerView recyclerView;
+    private Map<String, Object> data;
+    private InvoiceData invoice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_invoice_history);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        fullEventList = new ArrayList<>();
+        recyclerView = findViewById(R.id.invoice_history_recycler_view);
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("invoice").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    fullEventList.clear();
+                    for (DocumentSnapshot document : task.getResult()) {
+
+                        String eventID = document.getId();
+                        data = document.getData();
+                        String eventName = (String) data.get("event_name");
+                        String eventDate = (String) data.get("payment_date");
+                        String eventTime = (String) data.get("buyer_name");
+                        String ticketPrice = (String) data.get("ticket_price");
+                        String eventQty = (String) data.get("qty");
+                        String eventImage = (String) data.get("event_image");
+
+
+                        Log.i("EVENT CODE TEST", eventID);
+                        invoice = new InvoiceData(eventID, eventName, eventDate, eventTime, ticketPrice, eventQty, eventImage);
+                        fullEventList.add(invoice);
+                    }
+                    updateRecycler(fullEventList);
+
+                }
+            }
+
+
+        });
+
+    }
+
+    private void updateRecycler(ArrayList<InvoiceData> list) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new InvoiceAdapter(list));
+    }
+
+}
